719f2549f31b1f45698fe983f0baa07b709e3f18
Or Elimelech <0r3limelech@gmail.com>
2015-11-18 20:45:39 +0200

    typo

--- zsh/keys.zsh
+++ zsh/keys.zsh
@@ -1,2 +1,2 @@
 # Copy ssh public key to clipboard
-alias pubkey="cat ~/.ssh/id_id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="cat ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
