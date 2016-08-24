163c4279a34d7d647405af44c9a17e9ab73daba3
Or Elimelech <or.elimelech@blazemeter.com>
2016-08-24 13:42:09 +0300

    fixed completion

--- google/completion.zsh
+++
@@ -1,5 +0,0 @@
-if [[ $(uname) == "Darwin" ]]
-then
-	source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
-	source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
-fi
