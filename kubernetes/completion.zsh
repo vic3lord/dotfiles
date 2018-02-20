49c4b4f7e03db96a70ad1edc5764443fa9f32dda
Or Elimelech <0r3limelech@gmail.com>
2018-02-20 08:26:04 +0200

    my custom completion to change contexts

--- kubernetes/completion.zsh
+++ kubernetes/completion.zsh
@@ -2,3 +2,9 @@ if which kubectl > /dev/null 2>&1
 then
 	source <(kubectl completion zsh)
 fi
+
+function __k {
+	_arguments "1: :($(kubectl config get-contexts --output='name'))"
+}
+
+compdef __k k
