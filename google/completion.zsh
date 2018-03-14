4e59a9167ad3c9f393801a2ebfca22c12007ae88
Or Elimelech <0r3limelech@gmail.com>
2018-03-14 11:09:38 +0200

    google-chat

---
+++ google/completion.zsh
@@ -0,0 +1,7 @@
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+
+function __g {
+	_arguments "1: :($(gcloud config configurations list --format='value(name)'))"
+}
+
+compdef __g g
--- google/google.zsh
+++
@@ -1,1 +0,0 @@
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
