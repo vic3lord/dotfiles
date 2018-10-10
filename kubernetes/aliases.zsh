4a485185668fabd209cd8bf6bd4dd2b408589441
Or Elimelech <0r3limelech@gmail.com>
2018-10-10 14:38:25 +0300

    deprecate github.com/vic3lord/cronjobjob

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,2 +1,8 @@
 alias k='kubectl config use-context'
 alias kca='kubectl get --all-namespaces'
+
+function cronjobjob() {
+	local job=$1
+	local ns=${2:-default}
+	kubectl -n $ns create job --from=cronjob/$job
+}
