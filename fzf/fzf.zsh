de8869d8be1f34a44956a1b5d78202a7e20ecdb6
Or Elimelech <vic3lord@users.noreply.github.com>
2021-05-03 15:05:15 +0300

    update

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -4,6 +4,10 @@ source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 export FZF_TMUX=1
 
 # Solarized dark
+# export FZF_DEFAULT_OPTS="
+# 	--color=bg+:#3c3836,bg:#32302f,spinner:#fb4934,hl:#928374,fg:#ebdbb2,header:#928374,info:#8ec07c,pointer:#fb4934,marker:#fb4934,fg+:#ebdbb2,prompt:#fb4934,hl+:#fb4934'
+# "
+
 export FZF_DEFAULT_OPTS="
 	--color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
 	--color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54
