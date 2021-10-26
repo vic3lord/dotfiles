0df6f677be619f984e6384738548ec701863da36
Or Elimelech <vic3lord@users.noreply.github.com>
2021-10-26 23:48:08 +0300

    no need to guard for OS, brew is doing it for us

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,13 +1,4 @@
-#!/usr/bin/env sh
-#
-# install homebrew
-#
 if test ! $(which brew)
 then
-	if [ "$(uname)" == "Darwin" ]
-	then
-		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	else
-		echo "Unsupported OS: $(uname)"
-	fi
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
