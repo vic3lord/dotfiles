af88a3f36625a4e2df5aa471fee20292f0184424
Or Elimelech <vic3lord@users.noreply.github.com>
2021-10-26 23:54:29 +0300

    start transition to packer.nvim

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,9 +1,14 @@
 PLUG_PATH="~/.local/share/nvim/site/autoload/plug.vim"
 if [ ! -f $PLUG_PATH ]
-do
+then
 	curl -fLo $PLUG_PATH --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	nvim +PlugInstall +qall
-done
+fi
+
+if [ ! -f ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]
+then
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
 
 if [ ! -e ~/.config/nvim ]
 then
