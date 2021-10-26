241042a25fb0bfcfea9bd0e38d63882da9c5e4c7
Or Elimelech <vic3lord@users.noreply.github.com>
2021-10-26 23:50:26 +0300

    guard

--- macos/install.sh
+++ macos/install.sh
@@ -1,7 +1,5 @@
-# The Brewfile handles Homebrew-based app and library installs, but there may
-# still be updates and installables in the Mac App Store. There's a nifty
-# command line interface to it that we can use to just install everything, so
-# yeah, let's do that.
-
-echo "› macos softwareupdate"
-sudo softwareupdate -i -a
+if [ "$(uname)" == "Darwin" ]
+then
+	echo "› macos softwareupdate"
+	sudo softwareupdate -i -a
+fi
