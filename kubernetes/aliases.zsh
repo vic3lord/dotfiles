ba3c78794b2186c36e2521c16a8aacba54b9844b
Alon Gat <cybertoon@gmail.com>
2021-11-01 22:01:10 +0200

    add k8s prompt

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -8,10 +8,8 @@ function cronjobjob() {
 	kubectl -n $ns create job --from=cronjob/$job
 }
 
-alias spdemo="gcloud container clusters get-credentials sp-demo --region us-east1-b --project stackpulse-demo ; gcloud config set project stackpulse-demo ;set -e current_k8s_project"
-alias spdev="gcloud container clusters get-credentials sp-development --region europe-west1 --project stackpulse-development ; gcloud config set project stackpulse-development ;set -e current_k8s_project"
-alias spplaybooks="gcloud container clusters get-credentials sp-playbooks --region us-central1 --project stackpulse-playbooks ; gcloud config set project stackpulse-playbooks ; set -e current_k8s_project"
-alias spprd="gcloud container clusters get-credentials sp-production --region us-central1 --project stackpulse-production ; gcloud config set project stackpulse-production ; set -e current_k8s_project"
-alias spprod="gcloud container clusters get-credentials sp-production --region us-central1 --project stackpulse-production ; gcloud config set project stackpulse-production ; set -e current_k8s_project"
-alias spstage="gcloud container clusters get-credentials sp-staging --region europe-west1 --project stackpulse-staging ; gcloud config set project stackpulse-staging ; set -e current_k8s_project"
-alias spstg="gcloud container clusters get-credentials sp-staging --region europe-west1 --project stackpulse-staging ; gcloud config set project stackpulse-staging ; set -e current_k8s_project"
+alias spdemo="gcloud container clusters get-credentials sp-demo --region us-east1-b --project stackpulse-demo ; gcloud config set project stackpulse-demo"
+alias spdev="gcloud container clusters get-credentials sp-development --region europe-west1 --project stackpulse-development ; gcloud config set project stackpulse-development"
+alias spplaybooks="gcloud container clusters get-credentials sp-playbooks --region us-central1 --project stackpulse-playbooks ; gcloud config set project stackpulse-playbooks"
+alias spprd="gcloud container clusters get-credentials sp-production --region us-central1 --project stackpulse-production ; gcloud config set project stackpulse-production"
+alias spstg="gcloud container clusters get-credentials sp-staging --region europe-west1 --project stackpulse-staging ; gcloud config set project stackpulse-staging"
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,44 +1,7 @@
 autoload colors && colors
 
-git_branch() {
-  echo $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
-git_dirty() {
-  if $(! git status -s &> /dev/null)
-  then
-	echo ""
-  else
-	if [[ $(git status --porcelain --untracked-files=no) == "" ]]
-	then
-	    echo ":%{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-	else
-	    echo ":%{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-	fi
-  fi
-}
-
-git_prompt_info () {
- ref=$(git symbolic-ref HEAD 2>/dev/null) || return
- echo "${ref#refs/heads/}"
-}
-
-unpushed () {
-  git cherry -v @{upstream} 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-directory_name() {
-  echo "%{$fg[cyan]%}%1/%{$reset_color%}"
-}
-
 # set propmpt
 source ${0:A:h}/git-prompt.zsh
+source ${0:A:h}/gcloud-prompt.zsh
+
+RPROMPT='%{$fg[$ZSH_GCLOUD_PROMPT_COLOR]%}($ZSH_GCLOUD_PROMPT)%{$reset_color%}'
