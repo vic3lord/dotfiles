456abb6f1f4aea96c486b17a3621423a9aac3de3
Or Elimelech <vic3lord@users.noreply.github.com>
2021-12-13 16:36:32 +0200

    remove vim-plug

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,10 +1,3 @@
-PLUG_PATH="~/.local/share/nvim/site/autoload/plug.vim"
-if [ ! -f $PLUG_PATH ]
-then
-	curl -fLo $PLUG_PATH --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	nvim +PlugInstall +qall
-fi
-
 if [ ! -f ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]
 then
 	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
