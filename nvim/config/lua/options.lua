078266bb3bbc52bff78f87f08ab1ba874a4b328e
Or Elimelech <vic3lord@users.noreply.github.com>
2023-03-13 15:36:37 +0200

    lua opts

--- nvim/config/lua/options.lua
+++ nvim/config/lua/options.lua
@@ -1,27 +1,30 @@
-local opt = vim.opt
-opt.termguicolors = true
-opt.background = 'dark'
-opt.timeoutlen = 1000
-opt.ttimeoutlen = 0
-opt.number = true
-opt.relativenumber = true
-opt.numberwidth = 1
-opt.modeline = true
-opt.modelines = 5
-opt.laststatus = 2
-opt.undofile = true
-opt.showcmd = false
-opt.cursorline = true
-opt.cursorcolumn = true
-opt.autowrite = true
-opt.shortmess = opt.shortmess + 'c'
-opt.completeopt = {'menuone', 'noinsert', 'noselect'}
-opt.incsearch = true -- show search matches as you type
-opt.showmatch = true
-opt.hlsearch = true
-opt.smartcase = true -- ignore case if search pattern is all lowercase, case-sensitive otherwise
-opt.ignorecase = true
-opt.clipboard = opt.clipboard + 'unnamedplus' -- add clipboard support
+vim.opt.termguicolors = true
+vim.opt.background = 'dark'
+vim.opt.timeoutlen = 1000
+vim.opt.ttimeoutlen = 0
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 1
+vim.opt.modeline = true
+vim.opt.modelines = 5
+vim.opt.laststatus = 2
+vim.opt.undofile = true
+vim.opt.showcmd = false
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.opt.autowrite = true
+vim.opt.shortmess = vim.opt.shortmess + 'c'
+vim.opt.completeopt = {'menuone', 'noinsert', 'noselect'}
+vim.opt.incsearch = true -- show search matches as you type
+vim.opt.showmatch = true
+vim.opt.hlsearch = true
+vim.opt.smartcase = true -- ignore case if search pattern is all lowercase, case-sensitive otherwise
+vim.opt.ignorecase = true
+vim.opt.clipboard = vim.opt.clipboard + 'unnamedplus' -- add clipboard support
+
+-- Enable spell check
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = false
 
 -- Set colorscheme
 vim.cmd.colorscheme('gruvbox')
