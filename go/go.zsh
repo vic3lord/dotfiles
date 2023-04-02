72a42485ffd20525de44331830548fef88e55011
Or Elimelech <vic3lord@users.noreply.github.com>
2023-04-02 12:20:40 +0300

    wip

--- go/go.zsh
+++ go/go.zsh
@@ -1,3 +1,3 @@
 export GOPATH=$HOME/.go
 export GOPROXY=https://proxy.golang.org,direct
-export GOPRIVATE=github.com/torqio,stackpulse.dev,torqio.dev
+export GOPRIVATE=github.com/tanninio,tannin.dev
